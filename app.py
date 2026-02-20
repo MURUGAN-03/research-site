df3cf599e6eee82abffb9f882d88ffc731a2a78e
Muthu Lakshmi <murugansd2003@gmail.com>
2026-02-21 03:45:11 +0530

    fixed routes

--- app.py
+++ app.py
@@ -4,6 +4,27 @@ import os
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+# ---------------- ROUTES ---------------- #
+
+@app.route("/")
+def home():
+    return render_template("search.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/upload")
+def upload():
+    return render_template("upload.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
 app.secret_key = "supersecretkey"
 
 UPLOAD_FOLDER = 'uploads'
